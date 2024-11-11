243c96e52ca5b96a640a39d2c7e5c4e78a40d3fc
Lorenzo <lore95.lucchini@gmail.com>
2024-11-11 11:56:49 +0100

    Created setup of the project

---
+++ src/Project1Scripts.py
@@ -0,0 +1,12 @@
+
+# from PressureReadings.project.toolchain.projects.wiiboard.sensor_data.data_read import update
+# import tensorflow as tf
+# # import tensorflow_hub as hub
+# from tensorflow import keras
+import numpy as np
+import scipy as sp
+import Matplotlib as mp
+
+# Some modules to display an animation using imageio.
+from IPython.display import display
+
